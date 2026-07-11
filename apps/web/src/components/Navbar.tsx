e7fffe1b0094faf94d3c1803168515ec564dc1fd
AnahiSU <anahi7776@gmail.com>
2026-07-11 12:45:13 -0400

    Wrap Navbar title in a link to improve navigation

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar(){
     return (
         <nav className="bg-butter-500 text-white p-4">
-            <h1 className="text-2xl font-bold">Gustitos y Cafecitos</h1>
+            <a href="/"><h1 className="text-2xl font-bold">Gustitos y Cafecitos</h1></a>
         </nav>
     );
 }
