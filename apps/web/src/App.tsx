f6e5c6c098749547075745842e61e9cf56f6b61e
AnahiSU <anahi7776@gmail.com>
2026-07-11 11:57:17 -0400

    Add Navbar component and refactor App layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
-        <header className="bg-white shadow px-6 py-4">
-          <h1 className="text-2xl font-bold text-orange-600">Gustitos y Cafecitos ☕️</h1>
-        </header>
+      <div className="h-screen flex flex-col bg-gray-50 text-gray-900 font-sans">
+        <Navbar></Navbar>
 
-        <main className="p-6 max-w-4xl mx-auto">
+        <main className="mx-auto w-full flex-1 overflow-hidden">
           <Routes>
-            <Route path="/" element={Home()} />
+            <Route path="/" element={<Home />} />
             <Route path="/grupo/:slug" element={<h2>Vista de Grupo (Próximamente)</h2>} />
             <Route path="*" element={<h2>404 - No encontrado</h2>} />
           </Routes>
---
+++ apps/web/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return (
+        <nav className="bg-butter-500 text-white p-4">
+            <h1 className="text-2xl font-bold">Gustitos y Cafecitos</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
