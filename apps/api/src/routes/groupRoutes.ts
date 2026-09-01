c5ebcae5e2eb5f1c65379f0c37a03da417c939ee
Sergio Maldonado <sergiomaldonadosejas@gmail.com>
2026-08-31 21:40:28 -0400

    grandes cambios a front y re ajustes de rutas y formas en back, se arrelgaron bugs en back

--- apps/api/src/routes/groupRoutes.ts
+++ apps/api/src/routes/groupRoutes.ts
@@ -7,6 +7,9 @@ const router = Router();
 // POST /api/groups - Crear un grupo vacio (o con un creador)
 router.post('/',authenticateToken, groupController.create);
 
+// GET /api/groups/my-groups - Obtener los grupos del usuario logueado
+router.get('/my-groups', groupController.getMyGroups);
+
 // GET /api/groups/:slug - Obtener el grupo completo por su URL unica
 router.get('/:slug', groupController.getBySlug);
 
@@ -15,9 +18,6 @@ router.post('/:slug/restaurants', groupController.suggestRestaurant);
 
 // POST /api/groups/:groupId/members
 router.post('/:groupId/members', groupController.addMember);
-
-// GET /api/groups/my-groups - Obtener los grupos del usuario logueado
-router.get('/my-groups', groupController.getMyGroups);
 
 // GET /api/groups/:groupId/members - Obtener la lista de miembros de un grupo
 router.get('/:groupId/members', groupController.getMembers);
